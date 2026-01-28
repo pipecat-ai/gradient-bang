1c764c3f359c821d1d7e7857d3c87884712d1c1a
Jon Taylor <heyjtaylor@gmail.com>
2026-01-28 22:31:40 +0000

    ship assets

--- client/app/src/components/screens/ShipDetails.tsx
+++ client/app/src/components/screens/ShipDetails.tsx
@@ -2,11 +2,30 @@ import { useState } from "react"
 
 import { CircleNotchIcon } from "@phosphor-icons/react"
 
+import ImageAegisCruiser from "@/assets/images/ships/aegis_cruiser.png"
+import ImageAegisCruiserLogo from "@/assets/images/ships/aegis_cruiser_logo.png"
+import ImageAtlasHauler from "@/assets/images/ships/atlas_hauler.png"
+import ImageAtlasHaulerLogo from "@/assets/images/ships/atlas_hauler_logo.png"
+import ImageAutonomousLightHauler from "@/assets/images/ships/autonomous_light_hauler.png"
+import ImageAutonomousLightHaulerLogo from "@/assets/images/ships/autonomous_light_hauler_logo.png"
 import ImageAutonomousProbe from "@/assets/images/ships/autonomous_probe.png"
+import ImageAutonomousProbeLogo from "@/assets/images/ships/autonomous_probe_logo.png"
 import ImageBulwarkDestroyer from "@/assets/images/ships/bulwark_destroyer.png"
 import ImageBulwarkDestroyerLogo from "@/assets/images/ships/bulwark_destroyer_logo.png"
+import ImageCorsairRaider from "@/assets/images/ships/corsair_raider.png"
+import ImageCorsairRaiderLogo from "@/assets/images/ships/corsair_raider_logo.png"
 import ImageKestrel from "@/assets/images/ships/kestrel_courier.png"
 import ImageKestrelLogo from "@/assets/images/ships/kestrel_courier_logo.png"
+import ImagePikeFrigate from "@/assets/images/ships/pike_frigate.png"
+import ImagePikeFrigateLogo from "@/assets/images/ships/pike_frigate_logo.png"
+import ImagePioneerLifter from "@/assets/images/ships/pioneer_lifter.png"
+import ImagePioneerLifterLogo from "@/assets/images/ships/pioneer_lifter_logo.png"
+import ImageSovereignStarcruiser from "@/assets/images/ships/sovereign_starcruiser.png"
+import ImageSovereignStarcruiserLogo from "@/assets/images/ships/sovereign_starcruiser_logo.png"
+import ImageSparrowScout from "@/assets/images/ships/sparrow_scout.png"
+import ImageSparrowScoutLogo from "@/assets/images/ships/sparrow_scout_logo.png"
+import ImageWayfarerFreighter from "@/assets/images/ships/wayfarer_freighter.png"
+import ImageWayfarerFreighterLogo from "@/assets/images/ships/wayfarer_freighter_logo.png"
 import { useGameContext } from "@/hooks/useGameContext"
 import useGameStore from "@/stores/game"
 
@@ -15,12 +34,31 @@ import { Divider } from "../primitives/Divider"
 
 const SHIP_IMAGE_MAP = {
   autonomous_probe: ImageAutonomousProbe,
+  autonomous_light_hauler: ImageAutonomousLightHauler,
+  pioneer_lifter: ImagePioneerLifter,
+  sovereign_starcruiser: ImageSovereignStarcruiser,
+  atlas_hauler: ImageAtlasHauler,
   kestrel_courier: ImageKestrel,
   bulwark_destroyer: ImageBulwarkDestroyer,
+  wayfarer_freighter: ImageWayfarerFreighter,
+  corsair_raider: ImageCorsairRaider,
+  sparrow_scout: ImageSparrowScout,
+  aegis_cruiser: ImageAegisCruiser,
+  pike_frigate: ImagePikeFrigate,
 }
 const SHIP_LOGO_MAP = {
+  corsair_raider: ImageCorsairRaiderLogo,
+  pioneer_lifter: ImagePioneerLifterLogo,
+  sovereign_starcruiser: ImageSovereignStarcruiserLogo,
   kestrel_courier: ImageKestrelLogo,
+  atlas_hauler: ImageAtlasHaulerLogo,
   bulwark_destroyer: ImageBulwarkDestroyerLogo,
+  sparrow_scout: ImageSparrowScoutLogo,
+  autonomous_probe: ImageAutonomousProbeLogo,
+  autonomous_light_hauler: ImageAutonomousLightHaulerLogo,
+  wayfarer_freighter: ImageWayfarerFreighterLogo,
+  aegis_cruiser: ImageAegisCruiserLogo,
+  pike_frigate: ImagePikeFrigateLogo,
 }
 
 export const ShipDetails = ({ ship }: { ship: ShipDefinition }) => {
